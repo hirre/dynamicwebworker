cb4b8d7735cae4207485b7ff1b949739aa157777
Hirad Asadi <Hirad.Asadi@rco.se>
2024-07-12 11:35:12 +0200

    Updated fakework.

--- src/TestWork/SomeWork.cs
+++ src/TestWork/SomeWork.cs
@@ -7,8 +7,9 @@ namespace TestWork
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             // Do some important work here
-            Console.WriteLine("Super important work is being done here");
-            await Task.Delay(2000, stoppingToken);
+            Console.WriteLine("Super important work is being done here...");
+            await Task.Delay(5000, stoppingToken);
+            Console.WriteLine("Super important work done.");
         }
     }
 }
